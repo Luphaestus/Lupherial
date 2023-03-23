713a31812fef06d2c79f3ac10e0df754cde17be6
zak <zak.rosin@gmail.com>
2023-03-23 08:28:41 +0000

    Automatic Push

--- game/src/entities/types/item/inventory/shared_res/InventoryItem.gd
+++ game/src/entities/types/item/inventory/shared_res/InventoryItem.gd
@@ -6,12 +6,15 @@ var world_entity_path : String
 var stack_size : int
 var geometry
 
+var globalPosition = Vector2(-1, -1)
+
 @export var texture_ : TextureRect
 var real_scale = 0
 func init(new_size):
 	real_scale =  Vector2(new_size)/texture_.texture.get_size()
 	real_scale *= Vector2(len(geometry[0]), len(geometry))
 	texture_.scale = real_scale
+
 
 	
 func get_item_size():
