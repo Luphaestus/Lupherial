80f5aaac6e1617a6ae571e112f381430a105eb3b
zak <zak.rosin@gmail.com>
2023-03-25 10:11:50 +0000

    Automatic Push

--- compile.sh
+++ compile.sh
@@ -79,7 +79,7 @@ REPO_NAME="Lupherial"
 GITHUB_TOKEN="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"
 
 drawProgressBar 4 0 "Syncing with Github:"
-git fetch origin output.txt 2>&1
+git fetch origin output.txt 2>&1 
 drawProgressBar 4 1 "Syncing with Github:"
 git merge origin/main output.txt 2>&1
 drawProgressBar 4 2 "Syncing with Github:"
