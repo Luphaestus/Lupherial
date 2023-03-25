b90328ad193c529a7a0e42f79ac2011c5099258b
zak <zak.rosin@gmail.com>
2023-03-25 10:17:34 +0000

    Automatic Push

--- compile.sh
+++ compile.sh
@@ -78,15 +78,15 @@ REPO_OWNER="Luphaestus"
 REPO_NAME="Lupherial"
 GITHUB_TOKEN="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"
 
-drawProgressBar 4 0 "Syncing with Github:"
-git fetch origin > output.txt 2>&1
-drawProgressBar 4 1 "Syncing with Github:"
-git merge origin/main > output.txt 2>&1
-drawProgressBar 4 2 "Syncing with Github:"
-git commit -m "Automatic Push" > output.txt 2>&1
-drawProgressBar 4 3 "Syncing with Github:"
-git push https://${REPO_OWNER}:${GITHUB_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git main > output.txt 2>&1
-drawProgressBar 4 4 "Syncing with Github:"
+#drawProgressBar 4 0 "Syncing with Github:"
+git fetch origin #> output.txt 2>&1
+#drawProgressBar 4 1 "Syncing with Github:"
+git merge origin/main #> output.txt 2>&1
+#drawProgressBar 4 2 "Syncing with Github:"
+git commit -m "Automatic Push" #> output.txt 2>&1
+#drawProgressBar 4 3 "Syncing with Github:"
+git push https://${REPO_OWNER}:${GITHUB_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git main #> output.txt 2>&1
+#drawProgressBar 4 4 "Syncing with Github:"
 
 if $github; then
   echo "└──────────────────────────────────────────────────────────────────────────────────────────────────────┘"
