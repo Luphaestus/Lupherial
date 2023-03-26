d2448a6d013f065f8468c31e991ef44ec8686aae
zak <zak.rosin@gmail.com>
2023-03-26 18:53:33 +0100

    Automatic Push

--- game/menus/game_over/game_over.gd
+++ game/menus/game_over/game_over.gd
@@ -5,3 +5,4 @@ extends PanelContainer
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("UI_click") or Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://menus/title/title_screen.tscn")
+
